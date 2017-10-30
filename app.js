0b9f0da445b3d59bc41562a87f15cbcce4837100
bpark8 <bpark8@uncc.edu>
2017-10-30 02:33:30 -0400

    v1.0.0

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Trello = require('trello');
+const trello = new Trello(process.env.TRELLO_API_KEY, process.env.TRELLO_USER_TOKEN)
+// const request = require('superagent');
 
 client.on('ready', () => {
     console.log('Alred at your service');
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('aowdhaowhda');
+    var cmd = msg.content.substr(0, msg.content.indexOf(' '));
+    var idea = msg.content.substr(msg.content.indexOf(' ' ) + 1);
+
+    if (cmd === '!idea') {
+        trello.addCard(idea, '', '59b4f885bdeef2d997a0a21e', (err, card) => {
+            if (err) {
+                msg.reply("I couldn't create your card. Check your command and make sure it is in the format of !idea [message]");
+            } else {
+                msg.reply('I have gone ahead and added your card for you');
+            }
+        })
     }
 });
 
-client.login('Mzc0NDI5MDU5Mzg1NTI0MjM2.DNhJvw.yhWj8eH6iAsjoz5uoeUn9MkrIyg');
+client.login(process.env.DISCORD_BOT_TOKEN);
